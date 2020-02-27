f6a3f6ebab5bbd4c466c55cf9ba9f240c3e0eed5
Davit Badalyan <d.badalyan@mail.utoronto.ca>
2020-02-26 20:34:42 -0500

    Add shims
    Shims are decleration files that silence typescript

---
+++ usca-app/src/components/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calendar">
+    <vue-cal selected-date="2018-11-19"
+         :time-from="9 * 60"
+         :disable-views="['years', 'year']"
+         default-view="week"
+         events-on-month-view="short"
+         :events="events"
+         style="height: 600px">
+</vue-cal>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueCal from "vue-cal";
+import 'vue-cal/dist/vuecal.css'
+
+export default Vue.extend({
+  name: "calendar",
+  components: { VueCal },
+  props: {
+   
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
