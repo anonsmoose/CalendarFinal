c65f6a05b3a7d26b003e3085d5dfef487bfe76bf
amitohm1 <36339943+amitohm1@users.noreply.github.com>
2020-03-13 18:18:53 -0400

    Completed Scraping Courses
    added courses scraped data and the scripts used to generate them.

---
+++ course_data/scrap.py
@@ -0,0 +1,7 @@
+import uoftscrapers
+
+# Example: scrape http://map.utoronto.ca building data to ./some/path
+
+# Example: scrape http://coursefinder.utoronto.ca to current working directory
+
+uoftscrapers.Courses.scrape()
