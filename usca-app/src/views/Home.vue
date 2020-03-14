cb5db3b77861d31c44eb61dd9df7396c6e51c221
Anon Smith <ffxii.basch@gmail.com>
2020-03-13 22:56:52 -0400

    SYN-43 added a search bar

--- usca-app/src/views/Home.vue
+++ usca-app/src/views/Home.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="home">
-    <Calendar />
+    <Navbar />
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <Calendar />
+      </div>
+      <div class="column is-one-quarter">
+        <Searchbar />
+      </div>
+    </div>
   </div>
 </template>
+
+<!-- <template> -->
+<!--   <div class ="block"> -->
+<!--     <div class="notification"> -->
+<!--       lorem ip -->
+<!--     </div> -->
+<!--   </div> -->
+<!-- </template> -->
 
 <script>
 // @ is an alias to /src
 import Calendar from "@/components/Calendar.vue";
+import Searchbar from "@/components/SearchBar.vue";
 
 export default {
   name: "Home",
   components: {
-    Calendar
-  }
+    Calendar,
+    Searchbar,
+  },
 };
 </script>
