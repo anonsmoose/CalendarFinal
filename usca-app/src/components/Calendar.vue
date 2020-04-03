71c3c2791722c61919e4cad92d5827fbdddadd2f
Anon Smith <ffxii.basch@gmail.com>
2020-04-03 09:36:15 -0400

    repeat events over the semester

--- usca-app/src/components/Calendar.vue
+++ usca-app/src/components/Calendar.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="vuecal">
     <vue-cal
-      class="vuecal--blue-theme"
+      class="vuecal--green-theme"
       :time-from="8 * 60"
       :time-to="23 * 60"
       :disable-views="['years', 'year']"
       default-view="week"
       events-on-month-view="short"
       :events="events"
-      hide-weekends
       style="height: 600px"
     ></vue-cal>
   </div>
@@ -38,6 +37,7 @@ import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
 import { bus } from "../main";
 
+
 export default Vue.extend({
   name: "calendar",
   components: { VueCal },
@@ -46,7 +46,46 @@ export default Vue.extend({
   },
   data: () => ({
     events: [
-    ]
+      {
+        start: "2020-02-24 13:00",
+        end: "2020-02-24 14:00",
+        title: "PHL258H5 LEC0101"
+      },
+      // {
+      //   start: "2020-02-26 13:00",
+      //   end: "2020-02-26 15:00",
+      //   title: "PHL258H5 LEC0101"
+      // },
+      // {
+      //   start: "2020-02-25 13:00",
+      //   end: "2020-02-25 15:00",
+      //   title: "CSC301H5 LEC0101"
+      // },
+      // {
+      //   start: "2020-02-27 9:00",
+      //   end: "2020-02-27 10:00",
+      //   title: "CSC301H5 TUT0101"
+      // },
+      // {
+      //   start: "2020-02-25 15:00",
+      //   end: "2020-02-25 17:00",
+      //   title: "CSC309H5 LEC0101"
+      // },
+      // {
+      //   start: "2020-02-19 12:00",
+      //   end: "2020-02-19 14:00",
+      //   title: "LUNCH",
+      //   class: "lunch",
+      //   background: true
+      // },
+      // {
+      //   start: "2020-02-20 12:00",
+      //   end: "2020-02-20 14:00",
+      //   title: "LUNCH",
+      //   class: "lunch",
+      //   background: true
+      // }
+    ],
   }),
   methods:
   {
@@ -57,8 +96,116 @@ export default Vue.extend({
 
       console.log("inside the calendar");
       console.log(course);
-      this.events.push(course);
-    })
+      console.log(course.start);
+      // console.log(course.start.lastIndexOf("-"));
+      console.log(course.start.slice(0, 10));
+      if (course.title.charAt(8) == "F")
+      {
+        console.log("it doesn't really start at that date");
+        if (course.start.startsWith("2020-01-07"))
+        {
+             course.start = "2019-09-10"  + course.start.slice(10);
+             course.end = "2019-09-10" + course.end.slice(10);
+        }
+          else if (course.start.startsWith("2020-01-06"))
+        {
+             course.start = "2019-09-09"  + course.start.slice(10);
+             course.end = "2019-09-09" + course.end.slice(10);
+        }
+          else if (course.start.startsWith("2020-01-08"))
+        {
+             course.start = "2019-09-11"  + course.start.slice(10);
+             course.end = "2019-09-11" + course.end.slice(10);
+        }
+          else if (course.start.startsWith("2020-01-09"))
+        {
+             course.start = "2019-09-12"  + course.start.slice(10);
+             course.end = "2019-09-12" + course.end.slice(10);
+        }
+           else if (course.start.startsWith("2020-01-10"))
+        {
+             course.start = "2019-09-13"  + course.start.slice(10);
+             course.end = "2019-09-13" + course.end.slice(10);
+        }
+ 
+        // console.log(course);
+      }
+      // const startdate = new date(course.start.slice(0, 10));
+      // console.log(startDate);
+      // const endDate = new Date(course.end.slice(0, 10));
+
+      const startDate = new Date(course.start.slice(0, 10).replace("-", "/").replace("-", "/"));
+      console.log(startDate);
+      const endDate = new Date(course.end.slice(0, 10).replace("-", "/").replace("-", "/"));
+      // console.log(course.start.slice(0, 10).replace("-", "/").replace("-", "/"));
+      
+
+      // const date = new Date(startDate);
+      // startDate.setDate(startDate.getDate() + 7);
+      // console.log(startDate.toISOString().slice(0, 10) + " " + course.start.slice(11));
+      
+      // dateList.push(date)
+      // console.log(dateList);
+
+      // console.log(addWeeks(dt, 10).toString());
+
+      let i = 0;
+      while (i < 13)
+      {
+        const dict = {};
+
+        dict["title"] = course.title;
+        dict["background"] = true;
+        dict["class"] = "lunch";
+        dict["start"] = startDate.toISOString().slice(0, 10) + " " + course.start.slice(11);
+        dict["end"] = endDate.toISOString().slice(0, 10) + " " + course.end.slice(11);
+        startDate.setDate(startDate.getDate() + 7);
+        endDate.setDate(endDate.getDate() + 7);
+        i+=1;
+        this.events.push(dict);
+
+      }
+      console.log(this.events);
+
+    
+      
+
+      // dateList.push(5);
+      // console.log(dateList);
+      // dateList.push(1);
+      // console.log(dateList);
+      // dateList.push(9);
+      // console.log(dateList);
+      
+
+      // let i = 1;
+      // var startDateList = [];
+      // const endDateList = [];
+      // while (i < 14)
+      // {
+      //   // console.log(startDate);
+      //   // console.log(endDate);
+      //   startDate.setDate(startDate.getDate() + 7);
+      //   console.log(startDate);
+      //   startDateList.push(startDate);
+        
+      //   // endDate.setDate(endDate.getDate() + 7);
+      //   // startDateList.push(startDate);
+      //   // endDateList.push(endDate);
+      //   i+=1;
+      // } 
+      // console.log(startDateList);
+      // console.log(startDateList[1].toISOString());
+      // console.log(endDateList);
+
+
+      // const test = course.start.slice(8, 10)
+      // console.log(test);
+      // console.log(parseInt(test) + 5);
+      // console.log(('0' + '5').slice(-2));
+
+      // this.events.push(course);
+    });
   }
 });
 </script>
@@ -66,14 +213,15 @@ export default Vue.extend({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .vuecal__event {
-  background-color: #a8d6f7;
+  background-color: #fffacd;
   border-radius: 2px;
   color: #2c3e50;
-  font-size: 12px;
+  font-size: 10px;
   box-sizing: border-box;
   padding: 10px;
   margin-right: 20px;
 }
+
 .vuecal__now-line {display: none}
 </style>
 
